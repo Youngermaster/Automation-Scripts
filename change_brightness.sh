26147a94de2043068dc92245a7df04b42014be21
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-11-08 08:38:08 -0500

    Brightness script updated

--- change_brightness.sh
+++ change_brightness.sh
@@ -1,5 +1,5 @@
 # We use this command to get the display/s that we will change the brightness
 xrandr | grep " connected" | cut -f1 -d " "
 
-# We have to change the value for the brightness that we want
+# We have to change the value for the brightness that we want, from 0 to 2
 xrandr --output eDP --brightness 0.9
