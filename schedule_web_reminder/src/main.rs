dac543297f909eefce1a987cf6d587c87e5e53b8
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-01-17 21:58:29 -0500

    Rendering my own Schedule reminder

---
+++ schedule_web_reminder/src/main.rs
@@ -0,0 +1,31 @@
+fn main() -> wry::Result<()> {
+    use wry::{
+        application::{
+            event::{Event, StartCause, WindowEvent},
+            event_loop::{ControlFlow, EventLoop},
+            window::WindowBuilder,
+        },
+        webview::WebViewBuilder,
+    };
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Schedule Reminder")
+        .build(&event_loop)?;
+    let _webview = WebViewBuilder::new(window)?
+        .with_url("https://pond-part-0ba.notion.site/Schedule-aa43211f1458455f9a543c68ebeecb21")?
+        .build()?;
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
+}
